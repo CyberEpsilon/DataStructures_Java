550d4ab87c8473896f9180f86f346f90fed84923
Kathryn Woolard <shuricyber@gmail.com>
2020-06-25 17:55:10 -0400

    Basic Java program

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+
+	public static void main (String[] args){
+		System.out.println("Hello World");
+		System.out.println(AddIt(2,4));
+	}
+
+	public static int AddIt(int x, int y){
+
+		return x + y; 
+	}
+	 
+}
